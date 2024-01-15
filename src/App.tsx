91de10d2fcd5557c36061abb4eb8d89a63da3759
NatProgramer <rodrigezsantiago40@gmail.com>
2024-01-14 19:11:28 -0500

    separating footer to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import viteLogo from '../public/vite.svg'
 import './App.css'
+import Footer from './Components/Footer'
 
-function App () {
+export default function App () {
   const [count, setCount] = useState(0)
 
   return (
@@ -12,8 +13,8 @@ function App () {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite</h1>
-      <div className="card">
+       <h1>Vite</h1>
+         <div className="card">
         <button onClick={() => { setCount((count) => count + 1) }}>
           count is {count}
         </button>
@@ -21,11 +22,7 @@ function App () {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </>
   )
 }
-
-export default App
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,6 @@
+export default function Footer () {
+ return ( <footer className="read-the-docs">
+  Click on the Vite and React logos to learn more
+ </footer>
+ )
+}
